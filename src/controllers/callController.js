0ddcf27d41105139774f4cea3485843cc75de158
Ankit <ankitchaudri123@gmail.com>
2026-02-27 23:20:29 +0530

    second

--- src/controllers/callController.js
+++ src/controllers/callController.js
@@ -1,13 +1,16 @@
 import CallLog from "../models/CallLog.js";
-
 export const uploadCalls = async (req, res) => {
     try {
+        console.log("Incoming Data:", req.body);
+
         const calls = req.body;
 
         await CallLog.insertMany(calls);
 
         res.status(200).json({ message: "Calls saved successfully" });
+
     } catch (error) {
+        console.log("Error saving calls:", error);
         res.status(500).json({ error: error.message });
     }
 };
