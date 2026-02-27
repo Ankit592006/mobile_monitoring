6e2a58b29fb1856358ec9042900e53b58e196be6
Ankit Chaudhari <ankitchaudri123@gmail.com>
2026-02-27 23:33:42 +0530

    third

--- src/app.js
+++ src/app.js
@@ -5,7 +5,8 @@ import callRoutes from "./routes/callRoutes.js";
 const app = express();
 
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ limit: "10mb", extended: true }));
 
 app.use("/api/calls", callRoutes);
 
